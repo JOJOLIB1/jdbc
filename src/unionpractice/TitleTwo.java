c12eaecf56bafca436b00de7f51d213845fc7ab1
江骏杰 <a6691893@163.com>
2022-06-22 16:34:46 +0800

    jdbc_day3 MySQL作业小改

--- src/unionpractice/TitleTwo.java
+++ src/unionpractice/TitleTwo.java
@@ -1,6 +1,7 @@
 package unionpractice;
 
 import bean.ExamStudent;
+import org.junit.Test;
 import preparedstatement_pack.GeneralQuery;
 import preparedstatement_pack.GeneralUpdateSql;
 
@@ -16,7 +17,7 @@ import java.util.Scanner;
  */
 public class TitleTwo {
     public static void main(String[] args) {
-        System.out.println("请输入您要输入的类型:");
+/*        System.out.println("请输入您要输入的类型:");
         System.out.println("a:准考证号");
         System.out.println("b:身份证号");
         Scanner scanner = new Scanner(System.in);
@@ -39,13 +40,28 @@ public class TitleTwo {
             System.out.println("查无此人,请重新输入");
         }else {
             query.forEach(System.out::println);
-        }/*        System.out.println("请输入学生的考证号");
+        }*//*        System.out.println("请输入学生的考证号");
         Scanner scanner = new Scanner(System.in);
         String s = scanner.next();
         GeneralUpdateSql gs = new GeneralUpdateSql();
         gs.update("delete from exam_student where ExamCard = ?",s);
         System.out.println("删除成功");*/
-
+        System.out.println("请输入考生名字");
+        Scanner s = new Scanner(System.in);
+        String name = s.next();
+        GeneralQuery gn = new GeneralQuery();
+        List<ExamStudent> query = gn.query(ExamStudent.class, "SELECT FlowId flowId FROM exam_student where StudentName = ?",name);
+        if (query.isEmpty()) {
+            System.out.println("没有这个人");
+        }else {
+            GeneralUpdateSql gs = new GeneralUpdateSql();
+            String grade = s.next();
+            gs.update("insert into exam_student(grade) values(?)",grade);
+            System.out.println("插入成功");
+        }
+    }
+    @Test
+    public void test() {
 
     }
 
